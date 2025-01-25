65e249db99ed8a03b739c4e8ae80bbaa029b502a
mikatan-mikan <a200401021236@gmail.com>
2025-01-25 19:13:25 +0900

    set と delを仮組み

--- py-builder/repos/discord/command/terminal/common.py
+++ py-builder/repos/discord/command/terminal/common.py
@@ -7,6 +7,17 @@ from ....assets.text_dat import *
 from ....assets.utils import *
 #!end-ignore
 
+command_group_terminal = app_commands.Group(name="terminal",description="terminal group")
+
+async def change_terminal_ch(channel: int | False):    
+    #terminalを無効化
+    where_terminal = channel
+    config["discord_commands"]["terminal"]["discord"] = where_terminal
+    terminal_logger.info(f"terminal setting -> {where_terminal}")
+    #configを書き換え
+    with open(now_path + "/.config","w") as f:
+        json.dump(config,f,indent=4,ensure_ascii=False)
+
 #!open ./repos/discord/command/terminal/set.py
 #!ignore
 from .set import *
@@ -16,3 +27,4 @@ from .set import *
 from .delete import *
 #!end-ignore
 
+tree.add_command(command_group_terminal)
--- py-builder/repos/discord/command/terminal/set.py
+++ py-builder/repos/discord/command/terminal/set.py
@@ -8,24 +8,19 @@ from ....assets.utils import *
 from .common import *
 #!end-ignore
 
-
+terminal_set_logger = terminal_logger.getChild("set")
 
 #/terminal
-@tree.command(name="terminal",description=COMMAND_DESCRIPTION[lang]["terminal"])
-async def terminal(interaction: discord.Interaction):
+@command_group_terminal.command(name="set",description=COMMAND_DESCRIPTION[lang]["terminal"])
+async def terminal_set(interaction: discord.Interaction, channel:discord.TextChannel = None):
     global where_terminal
     await print_user(terminal_logger,interaction.user)
-    embed = ModifiedEmbeds.DefaultEmbed(title= f"/terminal")
+    embed = ModifiedEmbeds.DefaultEmbed(title= f"/terminal set {channel}")
     # 権限レベルが足りていないなら
     if await user_permission(interaction.user) < COMMAND_PERMISSION["terminal"]:
         await not_enough_permission(interaction,terminal_logger)
         return
     #発言したチャンネルをwhere_terminalに登録
-    where_terminal = interaction.channel_id
-    config["discord_commands"]["terminal"]["discord"] = where_terminal
-    terminal_logger.info(f"terminal setting -> {where_terminal}")
-    #configを書き換え
-    with open(now_path + "/.config","w") as f:
-        json.dump(config,f,indent=4,ensure_ascii=False)
+    change_terminal_ch(channel.id)
     embed.add_field(name="",value=RESPONSE_MSG["terminal"]["success"].format(where_terminal),inline=False)
     await interaction.response.send_message(embed=embed)
