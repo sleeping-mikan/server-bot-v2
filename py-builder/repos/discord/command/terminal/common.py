304f74268bc40efa6a0254c3115fb2b95d01eb0b
mikatan-mikan <a200401021236@gmail.com>
2025-01-25 19:04:37 +0900

    作業前にterminalのコードを\repos\discord\command\terminal\common.pyに移動

---
+++ py-builder/repos/discord/command/terminal/common.py
@@ -0,0 +1,18 @@
+#!ignore
+from ....imports import *
+from ....constant import *
+from ....logger.logger_create import *
+from ....minecraft.read_properties import *
+from ....assets.text_dat import *
+from ....assets.utils import *
+#!end-ignore
+
+#!open ./repos/discord/command/terminal/set.py
+#!ignore
+from .set import *
+#!end-ignore
+#!open ./repos/discord/command/terminal/delete.py
+#!ignore
+from .delete import *
+#!end-ignore
+
